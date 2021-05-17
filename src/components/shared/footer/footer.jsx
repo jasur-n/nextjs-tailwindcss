84652cfdd18f37dd3e2f3ad489dc34f6632b3a0c
normuradoff <normuradoff1@gmail.com>
2021-05-17 19:05:03 +0500

    refactor: add some semantic tags

---
+++ src/components/shared/footer/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Image from "next/image";
+
+const Footer = (props) => <footer></footer>;
+
+Footer.propTypes = {};
+
+Footer.defaultProps = {};
+
+export default Footer;
